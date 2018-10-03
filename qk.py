17d3642710d3c238650cb0f02e12ae768c9b04c7
Chad <cwise24@users.noreply.github.com>
2018-10-03 10:06:46 -0700

    Create qk.py

---
+++ qk.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json, cookielib, requests
+
+url='https://api.f5.com/auth/pub/sso/login/ihealth-api'
+headers = {'content-type': 'application/json', 'user-agent':'FSE_QKapi'}
+payload = {'user_id':'c.wise@f5.com', 'user_secret':'S@tcom2$7'}
+session = requests.session()
+r_token = session.post(url, headers=headers, data=json.dumps(payload))
+
+print "Auth Token ", r_token.status_code
+
+url1 = 'https://ihealth-api.f5.com/qkview-analyzer/api/qkviews'
+headers1 = {'Accept': 'application/vnd.f5.ihealth.api', 'user-agent':'FSE_QKapi'}
+payload1 = {'visible_in_gui': 'True'}
+fvar = {'qkview': open('support.qkview', 'rb')}
+r_Up = session.post(url1, headers=headers1, files=fvar, data=payload1)
+
+print "Upload Status ",r_Up.status_code
