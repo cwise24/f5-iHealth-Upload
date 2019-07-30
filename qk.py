4b89fbd0f04c76326a9abb7aa9214938e4c9d4da
Chad <cwise24@users.noreply.github.com>
2019-07-30 16:10:52 -0700

    loop for multiple file

--- qk.py
+++ qk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import json, cookielib, requests
+import json, cookielib, requests, os
 
 url='https://api.f5.com/auth/pub/sso/login/ihealth-api'
 headers = {'content-type': 'application/json', 'user-agent':'FSE_QKapi'}
@@ -10,10 +10,11 @@ r_token = session.post(url, headers=headers, data=json.dumps(payload))
 
 print "Auth Token ", r_token.status_code
 
-url1 = 'https://ihealth-api.f5.com/qkview-analyzer/api/qkviews'
-headers1 = {'Accept': 'application/vnd.f5.ihealth.api', 'user-agent':'FSE_QKapi'}
-payload1 = {'visible_in_gui': 'True'}
-fvar = {'qkview': open('support.qkview', 'rb')}
-r_Up = session.post(url1, headers=headers1, files=fvar, data=payload1)
-
-print "Upload Status ",r_Up.status_code
+for f in os.listdir('.'):
+    if f.endswitch('.qkview'):
+        url1 = 'https://ihealth-api.f5.com/qkview-analyzer/api/qkviews'
+        headers1 = {'Accept': 'application/vnd.f5.ihealth.api', 'user-agent':'FSE_QKapi'}
+        payload1 = {'visible_in_gui': 'True'}
+        fvar = {'qkview': open( f, 'rb')}
+        r_Up = session.post(url1, headers=headers1, files=fvar, data=payload1)
+        print "Upload Status ",r_Up.status_code
