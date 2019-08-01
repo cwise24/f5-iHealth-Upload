53e41b081eaf3581a6248e3c5fa9dbdc8e252852
Chad <cwise24@users.noreply.github.com>
2019-07-31 20:33:20 -0700

    variable for directory path

--- qk.py
+++ qk.py
@@ -5,6 +5,8 @@ import json
 #import http.cookiejar for python3
 import requests
 
+dir_path = "roles/qkview/files/"
+
 url = 'https://api.f5.com/auth/pub/sso/login/ihealth-api'
 headers = {'content-type': 'application/json', 'user-agent': 'FSE_QKapi'}
 payload = {'user_id': '<user id>', 'user_secret': '<password>'}
@@ -13,11 +15,11 @@ r_token = session.post(url, headers=headers, data=json.dumps(payload))
 
 print "Auth Token ", r_token.status_code
 
-for f in os.listdir('roles/qkview/files'):
+for f in os.listdir(dir_path):
     if f.endswitch('.qkview'):
         url1 = 'https://ihealth-api.f5.com/qkview-analyzer/api/qkviews'
         headers1 = {'Accept': 'application/vnd.f5.ihealth.api', 'user-agent': 'FSE_QKapi'}
         payload1 = {'visible_in_gui': 'True'}
-        fvar = {'qkview': open('roles/qkview/files/'+ f, 'rb')}
+        fvar = {'qkview': open(dir_path + f, 'rb')}
         r_Up = session.post(url1, headers=headers1, files=fvar, data=payload1)
         print "Upload Status ",r_Up.status_code
